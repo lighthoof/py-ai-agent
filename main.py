710b68488d672cbcf4d54d2edf48d23c23a7520f
lighthoof <andrew.kuligin@gmail.com>
2026-01-20 20:53:28 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+
+
+def main():
+    load_dotenv()
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("Gemini API key is not present in configuration")
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    model = 'gemini-2.5-flash'
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(model=model, contents=args.user_prompt)
+    if response.usage_metadata is None:
+        raise RuntimeError("Something went wrong, please check your request")
+
+    print("User prompt: ", args.user_prompt)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
