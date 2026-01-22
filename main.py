dfbb2f623077b5970fe12c6b4859dd30844ff033
lighthoof <andrew.kuligin@gmail.com>
2026-01-22 20:58:35 +0200

    multiple message base + calc app

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 import argparse
 from dotenv import load_dotenv
-from google import genai
-
+from google.genai import types
+from google.genai import Client
 
 def main():
     load_dotenv()
@@ -13,17 +13,20 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     model = 'gemini-2.5-flash'
-    client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model=model, contents=args.user_prompt)
+    client = Client(api_key=api_key)
+    response = client.models.generate_content(model=model, contents=messages)
     if response.usage_metadata is None:
         raise RuntimeError("Something went wrong, please check your request")
 
-    print("User prompt: ", args.user_prompt)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print("User prompt: ", args.user_prompt)
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response:")
     print(response.text)
 
